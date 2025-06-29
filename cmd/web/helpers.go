6eb9ad24d31e0a1c9dd7f84389d7a0688698c017
muerewa <ffsocietyy@protonmail.com>
2025-06-29 18:32:35 +0300

    added csrf protection

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/form"
+	"github.com/justinas/nosurf"
 	"net/http"
 	"time"
 )
@@ -49,6 +50,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/form v3.1.4+incompatible
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/justinas/alice v1.2.0
+	github.com/justinas/nosurf v1.2.0
 	golang.org/x/crypto v0.39.0
 )
 
