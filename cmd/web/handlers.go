e96795b6874fa891b1705ea6f521177d730f70dc
muerewa <ffsocietyy@protonmail.com>
2025-07-02 14:15:51 +0300

    improved redirect after login

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -203,6 +203,12 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 
 	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
 
+	path := app.sessionManager.PopString(r.Context(), "originalPage")
+	if path != "" {
+		http.Redirect(w, r, path, http.StatusSeeOther)
+		return
+	}
+
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
